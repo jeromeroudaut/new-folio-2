8b3baa2de14a51d0c01fc0b6616a042b6361e019
johnmilner <john.milner@mac.com>
2018-07-17 20:54:33 -0700

    work on implementing three.js video, pt.O

--- src/static/js/demo3.js
+++ src/static/js/demo3.js
@@ -89,44 +89,48 @@ function init() {
 	// texture = new THREE.Texture(canvas);
 	// texture.minFilter = texture.magFilter = THREE.LinearFilter;
 
-	//texture = new THREE.TextureLoader().load( '../../static/media/vid/clouds.mp4');
-	video = document.querySelector( '.myVideo' );
-	texture = new THREE.TextureLoader().load( video );
-
+	texture = new THREE.TextureLoader().load( '../../static/media/img/home/case/clouds.jpg');
 	// texture = new THREE.TextureLoader().load( '../../static/media/img/home/case/archi.jpg');
 	texture.minFilter = texture.magFilter = THREE.LinearFilter;
 	//texture.minFilter = THREE.LinearFilter;
 
-	///////////
-	// VIDEO //
-	///////////
-	
 	// create the video element
-	// video = document.createElement( 'video' );
-	// // video.id = 'video';
-	// // video.type = ' video/ogg; codecs="theora, vorbis" ';
-	// video.src = "../../static/media/vid/clouds.mp4";
-	// video.load(); // must call after setting/changing source
-	// video.play();
+	video = document.createElement( 'video' );
+	// video.id = 'video';
+	// video.type = ' video/ogg; codecs="theora, vorbis" ';
+	video.src = "../../static/media/vid/mtns.mp4";
+	video.load(); // must call after setting/changing source
+	video.play();
 	
-	// // alternative method -- 
-	// // create DIV in HTML:
-	// // <video id="myVideo" autoplay style="display:none">
-	// //		<source src="videos/sintel.ogv" type='video/ogg; codecs="theora, vorbis"'>
-	// // </video>
-	// // and set JS variable:
-	// // video = document.getElementById( 'myVideo' );
+	// alternative method -- 
+	// create DIV in HTML:
+	// <video id="myVideo" autoplay style="display:none">
+	//		<source src="videos/sintel.ogv" type='video/ogg; codecs="theora, vorbis"'>
+	// </video>
+	// and set JS variable:
+	// video = document.getElementById( 'myVideo' );
 	
-	// videoImage = document.createElement( 'canvas' );
-	// videoImage.width = 480;
-	// videoImage.height = 204;
-	// videoImageContext = videoImage.getContext( '2d' );
-	// // background color if no video present
-	// videoImageContext.fillStyle = '#000000';
-	// videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
-	// videoTexture = new THREE.Texture( videoImage );
-	// videoTexture.minFilter = THREE.LinearFilter;
-	// videoTexture.magFilter = THREE.LinearFilter;
+	videoImage = document.createElement( 'canvas' );
+	videoImage.width = 480;
+	videoImage.height = 204;
+	videoImageContext = videoImage.getContext( '2d' );
+	// background color if no video present
+	videoImageContext.fillStyle = '#000000';
+	videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
+	videoTexture = new THREE.Texture( videoImage );
+	videoTexture.minFilter = THREE.LinearFilter;
+	videoTexture.magFilter = THREE.LinearFilter;
+	
+	var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
+	// the geometry on which the movie will be displayed;
+	// 		movie image will be scaled to fit these dimensions.
+	var movieGeometry = new THREE.PlaneGeometry( 240, 100, 4, 4 );
+	var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
+	movieScreen.position.set(0,50,0);
+	scene.add(movieScreen);
+	
+	camera.position.set(0,150,300);
+	camera.lookAt(movieScreen.position);
 
 
 	material = new THREE.MeshPhongMaterial( {
